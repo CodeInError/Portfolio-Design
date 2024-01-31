a2d6220686c03447d63baec4ca0017722b6ab306
Sameer Singh <ssingh@markettime.com>
2024-01-31 17:31:23 +0530

    custom-design page design start

--- src/app/components/custom-design/custom-design.component.ts
+++ src/app/components/custom-design/custom-design.component.ts
@@ -27,11 +27,17 @@ export class CustomDesignComponent implements OnInit {
                       '#dd7373','#aea3b0', '#29335c', '#114b5f', '#028090', '#a23b72', '#eaf0ce','#c0c5c1', '#7d8491',
                       '#f7d4bc'];
   gradientColors: string[] = [
-                        'linear-gradient(90deg, #3a3844, #4d4730)',
-                        'linear-gradient(90deg, #f4ead8, #cf6b87)',
-                        'linear-gradient(90deg, #5d2a42, #71719a)'
+                        'linear-gradient(109.1deg, rgb(181, 73, 91) 7.1%, rgb(225, 107, 140) 86.4%)',
+                        'linear-gradient(69.9deg, rgb(76, 79, 106) 3.2%, rgb(118, 124, 163) 97.6%)',
+                        'linear-gradient(179.4deg, rgb(12, 20, 69) -16.9%, rgb(71, 30, 84) 119.9%)',
+                        'linear-gradient(0deg, rgb(161, 140, 209) 0%, rgb(251, 194, 235) 100%)',
+                        'radial-gradient(939px at 94.7% 50%, rgb(0, 178, 169) 0%, rgb(0, 106, 101) 76.9%)',
+                        'linear-gradient(90deg, rgb(43, 77, 130) 0%, rgb(40, 144, 172) 100%)',
+                        'linear-gradient(45deg, rgb(217, 134, 15) 0%, rgb(225, 99, 66) 100%)',
+                        'linear-gradient(135deg, rgb(245, 177, 77) 0%, rgb(237, 53, 115) 100%)'
                       ];
   selectedGradient: string | any;
+gradientL: any;
 
   constructor(private router: Router) { }
 
